3891e1d6c5479054c78db5500ecbe9d70f3a6ac9
David Zarcó <davidzarcofca@gmail.com>
2022-03-29 02:39:05 -0300

    Creacion de clases ejercicio 1

---
+++ src/main/java/ar/edu/uner/fcad/ed/ejercicio1/Array00.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ar.edu.uner.fcad.ed.ejercicio1;
+
+/**
+ *
+ * @author David Zarcó
+ */
+public class Array00 {
+    
+}
---
+++ src/main/java/ar/edu/uner/fcad/ed/ejercicio1/ClienteArray00.java
@@ -0,0 +1,13 @@
+
+package ar.edu.uner.fcad.ed.ejercicio1;
+
+/**
+ *
+ * @author David Zarcó
+ */
+public class ClienteArray00 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
