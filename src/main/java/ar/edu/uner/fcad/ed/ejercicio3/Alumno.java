599fa4a7cc37ca0630c75fbaec1772303b76e0dd
Stefano-Kiwi <stefanobudon@gmail.com>
2022-04-05 15:37:31 -0300

    Arreglo pequeno error tipeo Alumno

--- src/main/java/ar/edu/uner/fcad/ed/ejercicio3/Alumno.java
+++ src/main/java/ar/edu/uner/fcad/ed/ejercicio3/Alumno.java
@@ -65,10 +65,10 @@ public class Alumno {
         return notaFinal;
     }
     private void calcularNotaFinal(){
-            double ex1 = (examen1*0.25);
+        double ex1 = (examen1*0.25);
         double ex2 = (examen2*0.30);
         double ex3 = (examen3*0.45);
-        notaFinal = (int) Math.ceil(ex1+ex2+ex3);      
+        notaFinal = (int) Math.ceil(ex1+ex2+ex3);     
         switch(notaFinal){
             case 1,2,3:
                 notaConcep= "Insuficiente";
